207160a8fb22108bb8d68656802aa63cbff9c330
Antonín Kučera <81852696+Limit-sest@users.noreply.github.com>
2025-11-20 21:34:19 +0100

    Add projects

---
+++ src/content.config.ts
@@ -0,0 +1,16 @@
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
+
+const projects = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/pages/projects" }),
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      tags: z.array(z.string()),
+      year: z.number(),
+      description: z.string(),
+      image: image(),
+    }),
+});
+
+export const collections = { projects };
---
+++ src/pages/projects/orbiter.md
@@ -0,0 +1,10 @@
+---
+name: orbiter
+tags: [.NET]
+year: 2025
+description: A terminal application for simulating the solar system.
+image: ../../data/projects/orbiter.png
+---
+
+# orbiter
+This is a terminal application for simulating the solar system, made for the Siege hackclub event.
