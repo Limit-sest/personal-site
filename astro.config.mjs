28f43153982394e9041784e525c7e5f18ab4571d
Antonín Kučera <81852696+Limit-sest@users.noreply.github.com>
2026-04-28 09:00:39 +0200

    Improve SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://kucera.dev",
   vite: { plugins: [tailwindcss()] },
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
 });
